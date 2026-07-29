e23460a96964692cec0b53d540e4a55a9408d92f
Hristo Kolev <hristo.kolev@sistechnology.bg>
2026-07-29 17:08:30 +0300

    removing a small bug

--- GoalService/src/test/java/com/GoalService/GoalService/GoaServiceApplicationTests.java
+++ GoalService/src/test/java/com/GoalService/GoalService/GoaServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GoaServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
